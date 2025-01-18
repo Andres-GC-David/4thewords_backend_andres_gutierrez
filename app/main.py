30f298998e22e08e2263dca469e4bbf448c147be
ANDRES.GUTIERREZCORALES@ucr.ac.cr <ANDRES.GUTIERREZCORALES@ucr.ac.cr>
2025-01-18 13:41:21 -0600

    Modificacion del archivo main para que se pueda utilizar la carpeta de imagenes y poder buscar las imagenes en esta carpeta

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
+
+app.mount("/imagenes", StaticFiles(directory="imagenes"), name="imagenes")
 
 @app.get("/")
 async def root():
